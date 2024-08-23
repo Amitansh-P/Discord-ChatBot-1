3fb716fb87c0480d12872ab0a4ffeace81288f25
Amitansh-P <156566286+Amitansh-P@users.noreply.github.com>
2024-08-23 15:16:11 +0530

    Created gitignore and fix some bugs

---
+++ commands/youtube.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'youtube',
+    description: "Sends youtube Links!",
+    execute(message, args){
+        message.channel.send('https://www.youtube.com/user/abrafomudit');
+    }
+}
